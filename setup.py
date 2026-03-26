b189ee1055fb59210c9e628a481464ee648c5031
Sonu <sahanisonu476@gmail.com>
2026-03-26 15:58:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.1",
+    author="sonu Sahani",
+    author_email="sahanisonu00476@gmail.com",  # ✅ comma added
+    packages=find_packages(),
+    install_requires=[]
+)
